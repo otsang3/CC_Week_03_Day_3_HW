38197cdd5865a2e9d560c803b8e0b7944544aee4
Oscar Tsang <ohhtsang@gmail.com>
2020-03-25 16:26:29 +0000

    created drop/create tables in cd.sql file

---
+++ db/cd.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS artists;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE artists (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE albums (
+  title VARCHAR(255),
+  genre VARCHAR(255),
+  artist_id INT REFERENCES artists(id)
+);
