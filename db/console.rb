d56c60eb56d37b39a2f881e9e03d0ef84fd10a42
Oscar Tsang <ohhtsang@gmail.com>
2020-03-25 16:47:16 +0000

    added artist1 and album1 to console.rb

--- db/console.rb
+++ db/console.rb
@@ -1,3 +1,12 @@
 require('pry')
-require_relative('../models/albums')
+require_relative('../models/album')
 require_relative('../models/artist')
+
+artist1 = Artist.new({'name' => 'J.Cole'})
+
+album1 = Album.new({'name' => '2014 Forrest Hills Drive',
+                    'genre' => 'Hip Hop',
+                    'artist_id' => artist1.id})
+
+binding.pry
+nil
--- models/album.rb
+++ models/album.rb
@@ -1,10 +1,12 @@
 class Album
   attr_accessor :name, :genre, :artist_id
+  attr_reader :id
 
   def initialize(options)
     @name = options['name']
     @genre = options['genre']
     @artist_id = options['artist_id']
+    @id = options['id'].to_i if options['id']
   end
 
 end
